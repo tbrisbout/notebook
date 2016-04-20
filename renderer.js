402ae71977306cea9b70e04ce4801b5faf300fa5
Thomas BRISBOUT <thomas.brisbout@gmail.com>
2016-04-20 06:16:22 +0200

    edit list title

--- renderer.js
+++ renderer.js
@@ -7,16 +7,19 @@
   const notes = remote.getGlobal('notes')
 
   const ul = $('#list')
+
   ul.innerHTML = notes.reduce((a, note, i) =>
     `${a}<li id="${i}" class="list-group-item" contenteditable>${note.title}</li>`, '')
 
-  ul.addEventListener('click', event => {
+  ul.addEventListener('click', ({ target: li }) => {
     const activeLi = $('li.selected')
     if (activeLi) activeLi.classList.remove('selected')
 
-    const li = event.target
     li.classList.add('selected')
-
     editor.value(notes[li.id].content)
+  })
+
+  ul.addEventListener('input', ({ target: li }) => {
+    notes[li.id].title = li.textContent
   })
 })()
