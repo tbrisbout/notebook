4a038b17dc66fc269918755660795123eb0add81
Thomas BRISBOUT <thomas.brisbout@gmail.com>
2016-04-20 06:25:56 +0200

    save notes in json file

--- renderer.js
+++ renderer.js
@@ -3,10 +3,12 @@
   const $ = document.querySelector.bind(document)
   const editor = new SimpleMDE({ toolbar: false })
 
+  const fs = require('fs')
   const { remote } = require('electron')
   const notes = remote.getGlobal('notes')
 
   const ul = $('#list')
+  const btn = $('#saveBtn')
 
   ul.innerHTML = notes.reduce((a, note, i) =>
     `${a}<li id="${i}" class="list-group-item" contenteditable>${note.title}</li>`, '')
@@ -21,5 +23,12 @@
 
   ul.addEventListener('input', ({ target: li }) => {
     notes[li.id].title = li.textContent
+  })
+
+  btn.addEventListener('click', () => {
+    notes[$('li.selected').id].content = editor.value()
+    fs.writeFile('./notes.json', JSON.stringify(notes), () => {
+      console.log('OK')
+    })
   })
 })()
