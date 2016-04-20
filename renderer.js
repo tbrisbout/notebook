55e0bbfc86ee16f7b1a0cbd306f263b5472a6032
Thomas BRISBOUT <thomas.brisbout@gmail.com>
2016-04-20 06:03:20 +0200

    get notes in renderer and display list

--- renderer.js
+++ renderer.js
@@ -1,3 +1,11 @@
 (() => {
   'use strict'
+  const $ = document.querySelector.bind(document)
+
+  const { remote } = require('electron')
+  const notes = remote.getGlobal('notes')
+
+  const ul = $('#list')
+  ul.innerHTML = notes.reduce((a, note, i) =>
+    `${a}<li id="${i}" class="list-group-item" contenteditable>${note.title}</li>`, '')
 })()
