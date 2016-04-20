ebf685b0d3d61b527e7e69b5e9b457778ee813e0
Thomas BRISBOUT <thomas.brisbout@gmail.com>
2016-04-20 04:46:00 +0200

    add window options (size and icon) and basic management

--- main.js
+++ main.js
@@ -2,7 +2,17 @@
 
 const { app, BrowserWindow } = require('electron')
 
+let mainWindow
+
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow()
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: './icon.png',
+  })
+
   mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.on('closed', () => { mainWindow = null })
 })
+
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit() })
