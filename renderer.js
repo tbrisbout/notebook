40340717574f5f03accb96b201fcaf3733e9752d
Thomas BRISBOUT <thomas.brisbout@gmail.com>
2016-04-20 06:09:30 +0200

    create editor and show note by clicking on list item

--- renderer.js
+++ renderer.js
@@ -1,6 +1,7 @@
 (() => {
   'use strict'
   const $ = document.querySelector.bind(document)
+  const editor = new SimpleMDE({ toolbar: false })
 
   const { remote } = require('electron')
   const notes = remote.getGlobal('notes')
@@ -8,4 +9,14 @@
   const ul = $('#list')
   ul.innerHTML = notes.reduce((a, note, i) =>
     `${a}<li id="${i}" class="list-group-item" contenteditable>${note.title}</li>`, '')
+
+  ul.addEventListener('click', event => {
+    const activeLi = $('li.selected')
+    if (activeLi) activeLi.classList.remove('selected')
+
+    const li = event.target
+    li.classList.add('selected')
+
+    editor.value(notes[li.id].content)
+  })
 })()
